234135a2c6c269f3b5b7b823dd5fd3d08865429b
seenivasangopal <seeni3vasang@gmail.com>
2021-02-24 09:28:29 +0530

    branch updated by seenu

--- src/org/bank/Bank.java
+++ src/org/bank/Bank.java
@@ -4,6 +4,7 @@ public class Bank extends AxisBank{
 	private void printingMethod() {
 		// TODO Auto-generated method stub
 		System.out.println("printing method");
+		System.out.println("updated by seenu");
 	}
 
 }
